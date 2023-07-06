e8d635a55cfc9f577fecdc9373319a1c9b3b522b
crystina <crystinagonz@gmail.com>
2023-07-06 20:41:25 -0300

    agregando secciones

--- js/app.js
+++ js/app.js
@@ -1,117 +1,115 @@
-
-
 (function () {
     const listElements = document.querySelectorAll('.menu__item--show');
     const list = document.querySelector('.menu__links');
     const menu = document.querySelector('.menu__hamburguer');
-
+  
     const addClick = () => {
-        listElements.forEach(element => {
-            element.addEventListener('click', () => {
-
-
-                let subMenu = element.children[1];
-                let height = 0;
-                element.classList.toggle('menu__item--active');
-
-
-                if (subMenu.clientHeight === 0) {
-                    height = subMenu.scrollHeight;
-                }
-
-                subMenu.style.height = `${height}px`;
-
-            });
+      listElements.forEach(element => {
+        element.addEventListener('click', () => {
+          let subMenu = element.children[1];
+          let height = 0;
+          element.classList.toggle('menu__item--active');
+  
+          if (subMenu.clientHeight === 0) {
+            height = subMenu.scrollHeight;
+          }
+  
+          subMenu.style.height = `${height}px`;
         });
-    }
-
+      });
+    };
+  
     const deleteStyleHeight = () => {
-        listElements.forEach(element => {
-
-            if (element.children[1].getAttribute('style')) {
-                element.children[1].removeAttribute('style');
-                element.classList.remove('menu__item--active');
-            }
-
-        });
-    }
-
-
-    window.addEventListener('resize', () => {
-        if (window.innerWidth > 800) {
-            deleteStyleHeight();
-            if (list.classList.contains('menu__links--show'))
-                list.classList.remove('menu__links--show');
-
-        } else {
-            addClick();
+      listElements.forEach(element => {
+        if (element.children[1].style.height) {
+          element.children[1].style.height = '';
+          element.classList.remove('menu__item--active');
         }
-    });
-
-    if (window.innerWidth <= 800) {
+      });
+    };
+  
+    const handleResize = () => {
+      if (window.innerWidth > 800) {
+        deleteStyleHeight();
+        if (list.classList.contains('menu__links--show')) {
+          list.classList.remove('menu__links--show');
+        }
+      } else {
         addClick();
+      }
+    };
+  
+    const handleMenuClick = () => {
+      list.classList.toggle('menu__links--show');
+    };
+  
+    window.addEventListener('resize', handleResize);
+  
+    if (window.innerWidth <= 919) {
+      addClick();
     }
-
-    menu.addEventListener('click', () => list.classList.toggle('menu__links--show'));
-
-
-
-})();
-
-window.addEventListener('load', function () {
+  
+    menu.addEventListener('click', handleMenuClick);
+  })();
+  
+  window.addEventListener('load', function () {
     const grande = document.querySelector('.grande');
     const imgWidth = document.querySelector('.img').offsetWidth;
     let desplazamiento = 0;
-
+  
     function desplazarCarrusel() {
-        desplazamiento -= imgWidth;
-        grande.style.transform = `translateX(${desplazamiento}px)`;
-        if (desplazamiento <= -imgWidth) {
-            desplazamiento = 0;
-            grande.style.transform = `translateX(0)`;
-        }
+      desplazamiento -= imgWidth;
+      grande.style.transform = `translateX(${desplazamiento}px)`;
+      if (desplazamiento <= -imgWidth) {
+        desplazamiento = 0;
+        grande.style.transform = `translateX(0)`;
+      }
     }
-
+  
     setInterval(desplazarCarrusel, 3000); /* Cambio de imagen cada 3 segundos */
-});
-
-/*seccion servicios*/
-const images = document.querySelectorAll('.servicios__figure');
-function triggerAnimation(entries){
-    entries.forEach(entry =>{
-        const image = entry.target.querySelector('img');
-        image.classList.toggle('unset', entry.isIntersecting);
-    })
-}
-const options = {
-    root:null,
-    rootMargin:"0px",
-    threshold:1
-}
-
-const observer = new IntersectionObserver(triggerAnimation, options);
-images.forEach(image =>{
+  });
+  
+  /* Sección servicios */
+  const images = document.querySelectorAll('.servicios__figure');
+  
+  function triggerAnimation(entries) {
+    entries.forEach(entry => {
+      const image = entry.target.querySelector('img');
+      image.classList.toggle('unset', entry.isIntersecting);
+    });
+  }
+  
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1
+  };
+  
+  const observer = new IntersectionObserver(triggerAnimation, options);
+  
+  images.forEach(image => {
     observer.observe(image);
-})
-
-// Obtener referencias a los elementos del DOM
-const botonIndividual = document.getElementById('boton-individual');
-const botonFamiliar = document.getElementById('boton-familiar');
-const planIndividual = document.getElementById('individual');
-const planFamiliar = document.getElementById('familiar');
-
-// Función para mostrar el plan individual y ocultar el plan familiar
-const mostrarPlanIndividual = () => {
-  planIndividual.style.display = 'block';
-  planFamiliar.style.display = 'none';
-};
-
-// Función para mostrar el plan familiar y ocultar el plan individual
-const mostrarPlanFamiliar = () => {
-  planIndividual.style.display = 'none';
-  planFamiliar.style.display = 'block';
-};
-
-// Asignar los event listeners a los botones
-botonIndividual.addEventListener('click', mostrarPlanIndividual);
-botonFamiliar.addEventListener('click', mostrarPlanFamiliar);
+  });
+  
+  // Obtener referencias a los elementos del DOM
+  const botonIndividual = document.querySelector('.individual');
+  const botonFamiliar = document.querySelector('.familiar');
+  const planIndividual = document.getElementById('individual');
+  const planFamiliar = document.getElementById('familiar');
+  
+  // Función para mostrar el plan individual y ocultar el plan familiar
+  const mostrarPlanIndividual = () => {
+    planIndividual.style.display = 'block';
+    planFamiliar.style.display = 'none';
+  };
+  
+  // Función para mostrar el plan familiar y ocultar el plan individual
+  const mostrarPlanFamiliar = () => {
+    planIndividual.style.display = 'none';
+    planFamiliar.style.display = 'block';
+  };
+  
+  // Asignar los event listeners a los botones
+  botonIndividual.addEventListener('click', mostrarPlanIndividual);
+  botonFamiliar.addEventListener('click', mostrarPlanFamiliar);
+  
